7a7a38ca10bd4f9bc4826d544f5bfa3cf69f24b0
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2017-03-22 14:06:31 +0200

    Port a property from https://github.com/ploeh/KataTennis/blob/724b4cb397436e75cb0ce2e401edb6dc41b8b369/KataTennis/TennisProperties.fs

--- Hedgeploration.fsx
+++ Hedgeploration.fsx
@@ -15,6 +15,9 @@ open Hedgehog
 #load "paket-files/ploeh/dependency-rejection-samples/FSharp/BookingApi/MaîtreD.fs"
 open Ploeh.Samples.MaîtreD
 
+#load "paket-files/ploeh/KataTennis/KataTennis/Tennis.fs"
+open Ploeh.Katas.Tennis
+
 module Gen =
     let reservation =
         gen {
@@ -25,6 +28,17 @@ module Gen =
             return { Date = bookingDate
                      Quantity = positiveQty
                      IsAccepted = trueOrFalse }
+        }
+
+    let pointData =
+        let point =
+            Gen.item [Love; Fifteen; Thirty]
+        gen {
+            let! playerOnePoint = point
+            let! playerTwoPoint = point
+
+            return { PlayerOnePoint = playerOnePoint
+                     PlayerTwoPoint = playerTwoPoint }
         }
 
 // The properties below have been ported from
@@ -59,3 +73,27 @@ let ``tryAccept behaves correctly when it can't accept`` =
 
 Property.print ``tryAccept behaves correctly when it can accept``
 Property.print ``tryAccept behaves correctly when it can't accept``
+
+// The properties below have been ported from
+// https://github.com/ploeh/KataTennis/blob/724b4cb397436e75cb0ce2e401edb6dc41b8b369/KataTennis/TennisProperties.fs
+
+let ``Given player: <30 when player wins then score is correct`` =
+    property {
+        let! current     = Gen.pointData
+        let! winner      = Gen.item [PlayerOne; PlayerTwo]
+        let! playerPoint = Gen.item [Love; Fifteen]
+        let  current     = pointTo winner playerPoint current
+
+        let actual = scoreWhenPoints current winner
+
+        let expectedPlayerPoint =
+            current
+            |> pointFor winner
+            |> incrementPoint
+        let expected =
+            expectedPlayerPoint
+            |> Option.map (fun p -> current |> pointTo winner p |> Points)
+        return expected = Some actual
+    }
+
+Property.print ``Given player: <30 when player wins then score is correct``
