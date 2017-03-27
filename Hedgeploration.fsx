3352cb3ea628703b557d2ddd9a565d0a1caee151
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2017-03-27 12:57:11 +0300

    Use Hedgehog directly from NuGet.

--- Hedgeploration.fsx
+++ Hedgeploration.fsx
@@ -1,15 +1,6 @@
 open System
 
-#load "paket-files/fsprojects/FSharpx.Collections/src/FSharpx.Collections/Collections.fs"
-      "paket-files/fsprojects/FSharpx.Collections/src/FSharpx.Collections/LazyList.fsi"
-      "paket-files/fsprojects/FSharpx.Collections/src/FSharpx.Collections/LazyList.fs"
-      "paket-files/hedgehogqa/dotnet-hedgehog/Hedgehog/Numeric.fs"
-      "paket-files/hedgehogqa/dotnet-hedgehog/Hedgehog/Seed.fs"
-      "paket-files/hedgehogqa/dotnet-hedgehog/Hedgehog/Tree.fs"
-      "paket-files/hedgehogqa/dotnet-hedgehog/Hedgehog/Random.fs"
-      "paket-files/hedgehogqa/dotnet-hedgehog/Hedgehog/Shrink.fs"
-      "paket-files/hedgehogqa/dotnet-hedgehog/Hedgehog/Gen.fs"
-      "paket-files/hedgehogqa/dotnet-hedgehog/Hedgehog/Property.fs"
+#r "packages/Hedgehog/lib/net452/Hedgehog.dll"
 open Hedgehog
 
 #load "paket-files/ploeh/dependency-rejection-samples/FSharp/BookingApi/MaîtreD.fs"
